fe9d454555cf3eff833f664a03dfee1cbbc6ac86
tunealog <tunealog@gmail.com>
2020-07-11 12:36:22 +0900

    pygame_Project Ball Game

---
+++ pygame_project/3_ball_movement.py
@@ -0,0 +1,160 @@
+# Python Project
+
+# Title : Project Ball Game
+# Date : 2020-07-11
+# Creator : tunealog
+
+import os
+import pygame
+##################################################################
+# Basic Initialize
+pygame.init()
+
+# Screen Size
+screen_width = 640
+screen_height = 480
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Screen Title
+pygame.display.set_caption("Tune a Pang")
+
+# FPS
+clock = pygame.time.Clock()
+##################################################################
+
+# 1. User Initialize
+# Create File Path
+current_path = os.path.dirname(__file__)
+image_path = os.path.join(current_path, "images")
+
+# Create Background
+background = pygame.image.load(os.path.join(image_path, "background.png"))
+
+# Create Stage
+stage = pygame.image.load(os.path.join(image_path, "stage.png"))
+stage_size = stage.get_rect().size
+stage_height = stage_size[1]
+
+# Create Character
+character = pygame.image.load(os.path.join(image_path, "character.png"))
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height - stage_height
+
+# Character Move
+character_to_x = 0
+
+# Character Speed
+character_speed = 5
+
+# Create Weapon
+weapon = pygame.image.load(os.path.join(image_path, "weapon.png"))
+weapon_size = weapon.get_rect().size
+weapon_width = weapon_size[0]
+
+weapons = []
+
+# Weapon Speed
+weapon_speed = 10
+
+# Create Ball
+ball_images = [
+    pygame.image.load(os.path.join(image_path, "balloon1.png")),
+    pygame.image.load(os.path.join(image_path, "balloon2.png")),
+    pygame.image.load(os.path.join(image_path, "balloon3.png")),
+    pygame.image.load(os.path.join(image_path, "balloon4.png"))
+]
+
+# Ball Speed
+ball_speed_y = [-18, -15, -12, -9]
+
+balls = []
+balls.append({
+    "pos_x": 50,
+    "pos_y": 50,
+    "img_idx": 0,
+    "to_x": 3,
+    "to_y": -6,
+    "init_spd_y": ball_speed_y[0]
+})
+
+# Event Loop
+running = True
+while running:
+
+    # FPS Setup
+    dt = clock.tick(60)
+
+    # 2. Event Process
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                character_to_x -= character_speed
+            elif event.key == pygame.K_RIGHT:
+                character_to_x += character_speed
+            elif event.key == pygame.K_SPACE:
+                weapon_x_pos = character_x_pos + \
+                    (character_width/2) - (weapon_width / 2)
+                weapon_y_pos = character_y_pos
+                weapons.append([weapon_x_pos, weapon_y_pos])
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                character_to_x = 0
+
+    # 3. Locate Character
+    character_x_pos += character_to_x
+
+    if character_x_pos < 0:
+        character_x_pos = 0
+    elif character_x_pos > screen_width - character_width:
+        character_x_pos = screen_width - character_width
+
+    weapons = [[w[0], w[1] - weapon_speed] for w in weapons]
+
+    weapons = [[w[0], w[1]] for w in weapons if w[1] > 0]
+
+    for ball_idx, ball_val in enumerate(balls):
+        ball_pos_x = ball_val["pos_x"]
+        ball_pos_y = ball_val["pos_y"]
+        ball_img_idx = ball_val["img_idx"]
+
+        ball_size = ball_images[ball_img_idx].get_rect().size
+        ball_width = ball_size[0]
+        ball_height = ball_size[1]
+
+        if ball_pos_x <= 0 or ball_pos_x > screen_width - ball_width:
+            ball_val["to_x"] = ball_val["to_x"] * -1
+
+        if ball_pos_y >= screen_height - stage_height - ball_height:
+            ball_val["to_y"] = ball_val["init_spd_y"]
+        else:
+            ball_val["to_y"] += 0.5
+
+        ball_val["pos_x"] += ball_val["to_x"]
+        ball_val["pos_y"] += ball_val["to_y"]
+    # 4. Collision Process
+
+    # 5. Display to Screen
+    screen.blit(background, (0, 0))
+    for weapon_x_pos, weapon_y_pos in weapons:
+        screen.blit(weapon, (weapon_x_pos, weapon_y_pos))
+
+    for idx, val in enumerate(balls):
+        ball_pos_x = val["pos_x"]
+        ball_pos_y = val["pos_y"]
+        ball_img_idx = val["img_idx"]
+        screen.blit(ball_images[ball_img_idx], (ball_pos_x, ball_pos_y))
+
+    screen.blit(stage, (0, screen_height - stage_height))
+    screen.blit(character, (character_x_pos, character_y_pos))
+    # Update Display
+    pygame.display.update()
+
+# pygame Quit
+pygame.quit()
